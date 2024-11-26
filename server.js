30c1f3564e402ff13f26f7aff29dc577a8b6a3a3
Martin Jurík <mato.jurik1@gmail.com>
2024-11-26 22:27:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(bodyParser.json());
-app.use(express.static('public')); // Verejný priečinok na statické súbory
 
 // Endpoint na načítanie menu
 app.get('/menu', (req, res) => {
     fs.readFile('menu.json', 'utf8', (err, data) => {
         if (err) {
+            console.error('Chyba pri čítaní súboru menu.json:', err);
             res.status(500).send({ error: 'Nemôžem načítať menu.' });
         } else {
             res.send(JSON.parse(data));
@@ -23,11 +25,17 @@ app.get('/menu', (req, res) => {
 app.post('/menu', (req, res) => {
     fs.writeFile('menu.json', JSON.stringify(req.body, null, 2), (err) => {
         if (err) {
+            console.error('Chyba pri zapisovaní do súboru menu.json:', err);
             res.status(500).send({ error: 'Nemôžem uložiť menu.' });
         } else {
             res.send({ message: 'Menu bolo úspešne uložené.' });
         }
     });
+});
+
+// Endpoint na admin stránku
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'admin.html'));
 });
 
 // Spustenie servera
